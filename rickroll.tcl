fb8964638f57e26efcd31754357a870574b851dc
David McNett <nugget@flightaware.com>
2013-02-09 13:21:25 -0600

    Dont delay when emitting a totally blank line.

--- rickroll.tcl
+++ rickroll.tcl
@@ -20,40 +20,45 @@ proc main {} {
 	while {1} {
 		set buf [string trim [gets $fh]]
 
-		set lpos	0
-		set buflen	[string length $buf]
-		set lastchr [expr $buflen - 1]
+		if {$buf eq ""} {
+			puts ""
+		} else {
+			set lpos	0
+			set buflen	[string length $buf]
+			set lastchr [expr $buflen - 1]
 
-		for {set rpos 0} {$rpos < $buflen} {incr rpos} {
-			set chr [string range $buf $rpos $rpos]
+			for {set rpos 0} {$rpos < $buflen} {incr rpos} {
+				set chr [string range $buf $rpos $rpos]
 
-			if {[chr_is_delimeter $chr] || $rpos == $lastchr} {
-				if {[chr_is_delimeter $chr]} {
-					set end [expr $rpos - 1]
-				} else {
-					set end $rpos
-				}
-				set word [string range $buf $lpos $end]
-
-				if {[regexp {\{(\d+)\}} $word _ usec]} {
-					waitfor $usec
-					set last_output "delay"
-				} else {
-					puts -nonewline "$word"
-					if {$chr eq " "} {
-						puts -nonewline $chr
+				if {[chr_is_delimeter $chr] || $rpos == $lastchr} {
+					if {[chr_is_delimeter $chr]} {
+						set end [expr $rpos - 1]
+					} else {
+						set end $rpos
 					}
-					flush stdout
-					set last_output "text"
-					waitfor $delay_word
-				}
+					set word [string range $buf $lpos $end]
 
-				set lpos [expr $rpos + 1]
+					if {[regexp {\{(\d+)\}} $word _ usec]} {
+						waitfor $usec
+						set last_output "delay"
+					} else {
+						puts -nonewline "$word"
+						if {$chr eq " "} {
+							puts -nonewline $chr
+						}
+						flush stdout
+						set last_output "text"
+						waitfor $delay_word
+					}
+
+					set lpos [expr $rpos + 1]
+				}
 			}
-		}
-		puts ""
-		if {$last_output ne "delay"} {
-			waitfor $delay_line
+			puts ""
+
+			if {$last_output ne "delay"} {
+				waitfor $delay_line
+			}
 		}
 
 		if {[eof $fh]} {
