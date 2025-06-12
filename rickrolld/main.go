652400676e61d1e751adb681e2103d4166550adf
David McNett <contact@nugget.info>
2025-06-11 23:05:36 -0500

    Modern structure
    It compiles, but not much more to be said.

--- rickrolld/main.go
+++ rickrolld/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"os"
+	"os/signal"
+)
+
+var (
+	filename  string = "../lyrics.dat"
+	delayWord int    = 200
+	delayLine int    = 1000
+
+	logger      *slog.Logger
+	logLevel    *slog.LevelVar
+	target      string
+	matchLogger *slog.Logger
 )
 
 func singLine(line string) error {
@@ -16,12 +29,25 @@ func singLine(line string) error {
 	return nil
 }
 
-func main() {
-	filename := "lyrics.dat"
+func setupLogger(ctx context.Context, stdout io.Writer) {
+	logLevel = new(slog.LevelVar)
+
+	handlerOptions := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+	handler := slog.NewTextHandler(stdout, handlerOptions)
+	logger = slog.New(handler)
+}
+
+func run(ctx context.Context, stdout io.Writer, stderr io.Writer, getenv func(string) string, args []string) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
+	defer stop()
+
+	setupLogger(ctx, stdout)
 
 	b, err := os.ReadFile(filename)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	r := bufio.NewReader(bytes.NewBuffer(b))
@@ -30,7 +56,11 @@ func main() {
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			log.Fatal(line, prefix, err)
+			logger.Error("error reading line",
+				"line", line,
+				"prefix", prefix,
+				"error", err)
+			return err
 		}
 
 		if len(line) == 0 {
@@ -38,9 +68,20 @@ func main() {
 		} else {
 			err = singLine(string(line))
 			if err != nil {
-				log.Fatal(err)
+				return err
 			}
 		}
 
+	}
+	return nil
+}
+
+// main does as little as we can get away with.
+func main() {
+	ctx := context.Background()
+
+	if err := run(ctx, os.Stdout, os.Stderr, os.Getenv, os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
 }
